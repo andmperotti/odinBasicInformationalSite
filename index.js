9c8fa00f831beb45051330d7bc37eca52e39e364
Andrew Perotti <Perotti28@gmail.com>
2026-02-02 09:30:33 -0800

    feat: add server
    In addition to creating a server I've defined specific responses to users requests using the url property of their requests and respond with the appropriate html file.
    Also added the 8080 port for the server to run on.

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const index = fs.readFileSync("index.html");
 const about = fs.readFileSync("about.html");
 const contact = fs.readFileSync("contact-me.html");
 const error = fs.readFileSync("404.html");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end(index);
+  } else if (req.url === "/about") {
+    res.end(about);
+  } else if (req.url === "/contact-me") {
+    res.end(contact);
+  } else {
+    res.end(error);
+  }
+});
+
+server.listen(8080);
